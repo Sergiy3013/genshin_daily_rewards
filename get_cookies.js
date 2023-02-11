05e30cf0801c0ef024befe4addf9fd163bc0cca1
salem <Serhiy30131@gmail.com>
2023-02-11 20:29:15 +0200

    Bug fix

--- get_cookies.js
+++ get_cookies.js
@@ -3,7 +3,7 @@ var ask = confirm('Cookie: ' + cookie + '\n\nClick confirm to copy Cookie.'); if
 function start() {
     return "ltoken=" + getCookie("ltoken") + ";ltuid=" + getCookie("ltuid") + ";";
     function getCookie(name) {
-        const value = ";" + document.cookie;
+        const value = "; " + document.cookie;
         const parts = value.split("; " + name + "=");
         if (parts.length === 2) return parts.pop().split(';').shift();
     }
